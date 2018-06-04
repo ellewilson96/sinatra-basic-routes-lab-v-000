b31c8783ecf11f39dfc0393cd6c5213d5e20a08d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-04 14:45:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,19 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    @name = Name.all
     "My name is #{name}"
-    erb :'name/index.html.erb'
   end
 
   get '/hometown' do
-    @hometown = Hometown.all
     "My hometown is #{hometown}"
-    erb :'hometown/index.html.erb'
   end
 
   get '/favorite-song' do
     "My favorite song is #{favorite-song}"
-    erb :'favorite-song/index.html.erb'
   end
 end
