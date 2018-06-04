006c0625032e0c62b04df9caebd42837640c733f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-04 14:47:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,10 +5,16 @@ class App < Sinatra::Base
   get '/name' do
     "My name is "
   end
+end
 
+class App < Sinatra::Base
+  
   get '/hometown' do
     "My hometown is "
   end
+end
+
+class App < Sinatra::Base
 
   get '/favorite-song' do
     "My favorite song is "
