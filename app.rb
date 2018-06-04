24ab2ff1fa02e2b47b0b58ebda19e2ee2ea539e2
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-04 14:39:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    @name = Name.all
+    "My name is"
+  end
+
+  get '/hometown' do
+    @hometown = Hometown.all
+    "My hometown is"
+  end
+
+  get '/favorite-song' do
+    @favorite = Favorite
+  end
 end
